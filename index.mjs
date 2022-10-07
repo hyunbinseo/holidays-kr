8b5b912e5cce1f57b9d5ebd169ea14a40fc7caec
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-10-07 17:25:10 +0900

    feat: csv file generation

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import { writeFile } from 'node:fs/promises';
+import holidays from './data/2023.json' assert { type: 'json' };
+
+const year = 2023;
+
+const holidayArray = Object.entries(holidays);
+
+holidayArray.forEach(([date, subject]) => {
+	if (!/\d{4}-\d{2}-\d{2}/.test(date)) throw new Error('Incorrect date format');
+	if (typeof subject !== 'string') throw new Error('Incorrect subject format');
+});
+
+const csvLines = holidayArray.map(([date, subject]) => (`${subject},${date}`));
+
+await writeFile(`./data/${year}.csv`, '\ufeff' + ['Subject,Start date', ...csvLines].join('\n'));
