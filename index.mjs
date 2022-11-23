554d74953afd0b221992267d25d828705fa6bc81
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-11-23 20:54:34 +0900

    refactor: don't convert json to array

--- index.mjs
+++ index.mjs
@@ -3,14 +3,18 @@ import holidays from './data/2023.json' assert { type: 'json' };
 
 const year = 2023;
 
-const holidayArray = Object.entries(holidays);
-
-holidayArray.forEach(([date, subject]) => {
+for (const date in holidays) {
+	const subject = holidays[date];
 	if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) throw new Error('Incorrect date format');
 	if (typeof subject !== 'string') throw new Error('Incorrect subject format');
 	if (!subject) throw new Error('Subject should not be empty');
-});
+};
 
-const csvLines = holidayArray.map(([date, subject]) => (`${subject},${date}`));
-
-await writeFile(`./data/${year}.csv`, '\ufeff' + ['Subject,Start date', ...csvLines].join('\n'));
+await writeFile(`./data/${year}.csv`,
+	'\ufeff' // BOM 
+	+ 'Start date,Subject\n'
+	+ JSON.stringify(holidays)
+		.replaceAll('","', '\n')
+		.replaceAll('":"', ',')
+		.replaceAll(/{"|"}/g, ''),
+);
