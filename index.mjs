3c929a25e2c8749e4842de690e14f1c571af021f
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-11-26 18:22:31 +0900

    chore: improve preset check message

--- index.mjs
+++ index.mjs
@@ -4,7 +4,8 @@ import presets from './input/presets.json' assert { type: 'json' };
 import { generateCsv } from './script/csv.mjs';
 import { generateIcs, generateIcsEvents } from './script/ics.mjs';
 
-const year = 2023;
+/** @type {string} */
+const year = '2023';
 
 const id = ids[year];
 const dates = presets[year];
@@ -14,8 +15,9 @@ if (!dates || typeof dates !== 'object' || !Object.keys(dates).length) throw new
 
 for (const date in dates) {
 	const subject = dates[date];
-	if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) throw new Error('Invalid date format');
-	if (!subject || typeof subject !== 'string') throw new Error('Invalid subject format');
+	if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) throw new Error(`Invalid date format. ${date} should in YYYY-MM-DD format`);
+	if (date.substring(0, 4) !== year) throw new Error(`Invalid date value. ${date} should be a date in year ${year}`);
+	if (!subject || typeof subject !== 'string') throw new Error(`Invalid subject format. Subject of ${date} should be a truthy string`);
 };
 
 await writeFile(`./output/${year}.csv`, generateCsv(dates));
