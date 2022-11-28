9610fd9964a3a29682e0153780cbf63ad46f47b2
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-11-28 13:12:04 +0900

    feat: generate cumulated ics file

--- index.mjs
+++ index.mjs
@@ -1,19 +1,34 @@
-import { writeFile } from 'node:fs/promises';
+import { writeFileSync } from 'node:fs';
 import ids from './input/ids.json' assert { type: 'json' };
 import presets from './input/presets.json' assert { type: 'json' };
 import { checkHolidays } from './script/checkHolidays.mjs';
 import { generateCsv } from './script/generateCsv.mjs';
 import { generateIcs, generateIcsEvents } from './script/generateIcs.mjs';
 
-/** @type {number} */
-const year = 2023;
+const years = Object.keys(presets).map((year) => {
+	if (!/^2\d{3}$/.test(year)) throw new Error('Invalid year format');
+	return Number(year);
+});
 
-const id = ids[year];
-if (!id || typeof id !== 'number' || id.toString().length !== 13)
-	throw new Error('Invalid ID format. Use a value returned by Date.now()');
+const minYear = Math.min(...years);
+const maxYear = Math.max(...years);
 
-const preset = presets[year];
-checkHolidays({ preset, year });
+let cumulatedIcsEvents = '';
 
-await writeFile(`./output/${year}.csv`, generateCsv(preset));
-await writeFile(`./output/${year}.ics`, generateIcs(generateIcsEvents({ preset, id })));
+for (let year = minYear; year <= maxYear; year += 1) {
+	const id = ids[year];
+	if (!id || typeof id !== 'number' || id.toString().length !== 13)
+		throw new Error('Invalid ID format. Use a value returned by Date.now()');
+
+	const preset = presets[year];
+	checkHolidays({ preset, year });
+
+	const icsEvents = generateIcsEvents({ preset, id });
+	cumulatedIcsEvents += icsEvents;
+
+	if (year === maxYear) {
+		writeFileSync(`./output/${year}.csv`, generateCsv(preset));
+		writeFileSync(`./output/${year}.ics`, generateIcs(icsEvents));
+		writeFileSync('./output/basic.ics', generateIcs(cumulatedIcsEvents));
+	};
+};
