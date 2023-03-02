a4ae341754920a961351e04715e89661cd5e7e8e
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-03-02 15:25:31 +0900

    chore: convert to typescript (wip)

--- generate.ts
+++ generate.ts
@@ -1,19 +1,20 @@
 import { writeFileSync } from 'node:fs';
+import type { Year } from './index';
 import { y2022, y2023 } from './index.js';
-import { checkHolidays } from './scripts/check-holidays.js';
-import { generateCsv } from './scripts/generate-csv.js';
-import { generateIcs, generateIcsEvents } from './scripts/generate-ics.js';
+import { checkHolidays } from './scripts/check-holidays';
+import { generateCsv } from './scripts/generate-csv';
+import { generateIcs, generateIcsEvents } from './scripts/generate-ics';
 
-/** @type Object.<number, [Object<string, string>,number]> */
-const holidays = {
-	2023: [y2023, 1669289424786],
-	2022: [y2022, 1669509606092],
-};
+const holidays = [
+	[2023, [y2023, 1669289424786]],
+	[2022, [y2022, 1669509606092]],
+] satisfies Array<[number, [Year, number]]>;
 
 let cumulatedIcsEvents = '';
 
-for (const [year, [preset, id]] of Object.entries(holidays)) {
-	if (!/^2\d{3}$/.test(year)) throw new Error(`Invalid year format - ${year}`);
+for (const [year, [preset, id]] of holidays) {
+	if (!/^2\d{3}$/.test(year.toString()))
+		throw new Error(`Invalid year format - ${year}`);
 
 	if (!id || typeof id !== 'number' || id.toString().length !== 13)
 		throw new Error(`Invalid ID format - ${year}, ${id}`);
