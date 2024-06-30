517fff7fb50d68d91bae922a7d7853e630b28975
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-06-30 19:32:15 +0900

    fix: preset's nested array type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
-export type Preset = Readonly<Record<string, ReadonlyArray<string>>>;
-
-export type YearlyPresets = ReadonlyMap<number, [Preset, number]>;
+// Object.freeze() does not deep freeze the object.
+// Nested arrays can be altered using .push(), etc.
+// LINK src/holidays/presets.ts
+export type Preset = Readonly<
+	Record<
+		string,
+		string[] // Cannot use [string, ...string[]] type.
+	>
+>;
 
 export type CheckPreset = (year: number, preset: Preset) => void;
